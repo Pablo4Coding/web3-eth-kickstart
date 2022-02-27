ead4e4ef99903ce7d0e4e36d4914a7e9b22ccef6
Pablo Andres <devpabloandres@gmail.com>
2022-02-27 19:37:47 +0100

    chore(deploy): deploy script ready

---
+++ ethereum/deploy.js
@@ -0,0 +1,28 @@
+require("dotenv-flow").config();
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const Web3 = require("web3");
+const compiledFactory = require("./build/CampaignFactory.json");
+
+console.log(process.env.MNEMONIC_ACCOUNT);
+
+const provider = new HDWalletProvider(
+  process.env.MNEMONIC_ACCOUNT,
+  process.env.INFURA_ENDPOINT
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log("Attempting to deploy from account", accounts[0]);
+
+  const result = await new web3.eth.Contract(
+    JSON.parse(compiledFactory.interface)
+  )
+    .deploy({ data: compiledFactory.bytecode })
+    .send({ gas: "1000000", from: accounts[0] });
+
+  console.log("Contract deployed to", result.options.address);
+  provider.engine.stop();
+};
+deploy();
